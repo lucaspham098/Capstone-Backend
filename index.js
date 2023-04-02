af2ac85ac2d0d692b956c615c1214b74d4da199a
lucaspham098 <lucaspham098@gmail.com>
2023-04-02 00:30:50 -0400

    remove authentication logic from index.js

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const app = express()
 const cors = require('cors')
 const PORT = process.env.PORT ?? 5050
 const CLIENT_URL = process.env.CLIENT_URL
-const jsonSECRETKEY = process.env.jsonSECRETKEY
-const jwt = require('jsonwebtoken')
+
 
 app.use(cors({
     origin: CLIENT_URL
@@ -20,16 +19,7 @@ const signupRoute = require('./routes/signup')
 app.use('/login', loginRoute)
 app.use('/signup', signupRoute)
 
-const authenticateToken = (req, res) => {
-    const token = req.headers.authorization.split(" ")[1]
-    if (token == null) {
-        return res.status(401).send('Not authenticated. Please login')
-    }
-    jwt.verify(token, jsonSECRETKEY, (err, user) => {
-        if (err) return res.status(403).send('Not authorized')
-        req.user = user
-    })
-}
+
 
 
 app.get('/', (req, res) => {
@@ -40,4 +30,3 @@ app.listen(PORT, () => {
     console.log(`We Live on ${PORT}`)
 })
 
-module.exports = { authenticateToken }
