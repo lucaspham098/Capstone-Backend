7f8a9840ea0602b8012b2277ffa828071decaacf
lucaspham098 <lucaspham098@gmail.com>
2023-05-16 15:07:00 -0400

    hash password in seed file

--- seeds/user.js
+++ seeds/user.js
@@ -2,14 +2,17 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
+const bcrypt = require('bcrypt');
 exports.seed = async function (knex) {
     // Deletes ALL existing entries
     await knex('users').del();
+    const password = 'omg';
+    const hashedPassword = await bcrypt.hash(password, 10);
     await knex('users').insert([
         {
             id: '036fa1ff-e605-4776-bc46-622cad4c9ebd',
             username: 'lucaspham',
-            password: 'omg',
+            password: hashedPassword,
             name: 'Lucas'
         }
     ]);
